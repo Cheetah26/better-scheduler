c8665dfacf94ded318b14f5f38160c5d029c605f
Cheetah26 <scmongo@gmail.com>
2022-12-09 13:52:06 -0500

    Create mock 'add to schedule' button

--- src/routes/events/event-[name]/+page.svelte
+++ src/routes/events/event-[name]/+page.svelte
@@ -13,11 +13,36 @@
   <h2>Error</h2>
   <p>We couldn't find the requested event</p>
 {:else}
-  <h2>Event: {event.name}</h2>
-  <h3>Description:</h3>
-  <p>{event.desc}</p>
-  <h3>When:</h3>
-  <p>{event.times}</p>
-  <h3>Detailed Info:</h3>
-  <p>{event.info}</p>
+  <div>
+    <h2>Event: {event.name}</h2>
+    <div class="actions" style="float: right">
+      <button>Add to schedule</button>
+    </div>
+    <h3>Description:</h3>
+    <p>{event.desc}</p>
+    <h3>When:</h3>
+    <p>{event.times}</p>
+    <h3>Detailed Info:</h3>
+    <p>{event.info}</p>
+  </div>
 {/if}
+
+<style>
+  button {
+    background-color: var(--accent);
+    border: none;
+    margin: 0 0.5em;
+
+    color: var(--light-text);
+    padding: 0.25em;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  .actions {
+    background-color: var(--foreground);
+    padding: 10px;
+  }
+</style>
